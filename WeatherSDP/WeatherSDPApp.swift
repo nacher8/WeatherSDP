e1d18aa5f27b5b3913b805071a91b17ae1133533
Ignacio Hernaiz <nacho_celtic@hotmail.com>
2025-12-11 20:22:49 +0100

    Initial Commit

---
+++ WeatherSDP/WeatherSDPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSDPApp.swift
+//  WeatherSDP
+//
+//  Created by IGNACIO HERNAIZ IZQUIERDO on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSDPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
